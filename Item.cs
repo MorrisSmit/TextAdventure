930ead8c83a375fd0604c81dccdff0a0ff8977c0
Morris Smit <morris.smit@hotmail.com>
2018-06-13 09:42:54 +0200

    added item and inventory class

---
+++ Item.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ZuulCS
+{
+
+    class Item
+    {
+        private string _itemName;
+        private string description;
+        private float weight;
+
+        public string itemName
+        {
+            get { return this._itemName; }
+        }
+
+
+
+	    public Item()
+        {
+            _itemName = "Undefined";
+            description = "Undefined";
+            weight  = 0;
+	    }
+    }
+}
